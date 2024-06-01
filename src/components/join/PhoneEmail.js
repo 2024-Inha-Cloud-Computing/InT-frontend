05909cebdf4fde09b5fda29a7c74f376bdc69adb
wogus <ind07152@naver.com>
2024-06-01 20:18:51 +0900

    refactor : join 폴더 생성

--- src/components/join/PhoneEmail.js
+++ src/components/join/PhoneEmail.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 
-import good from "../assets/img/check_good.png";
-import bad from "../assets/img/check_bad.png";
+import good from "../../assets/img/check_good.png";
+import bad from "../../assets/img/check_bad.png";
 
 const PhoneEmail = () => {
   window.addEventListener("resize", () => {
