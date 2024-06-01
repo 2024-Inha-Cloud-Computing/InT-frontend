7be5f52e0837e7e6f5c72131e6277e36a933ba29
wogus <ind07152@naver.com>
2024-06-01 20:19:40 +0900

    feat : 취향 정보 입력1

---
+++ src/components/taste/Time.js
@@ -0,0 +1,48 @@
+import { useEffect } from "react";
+import "./Time.css";
+
+const Time = () => {
+  useEffect(() => {
+    sessionStorage.setItem("value", "");
+  });
+  const clickAm = (event) => {
+    event.preventDefault();
+    const value = [];
+    value.push(true);
+    sessionStorage.setItem("value", JSON.stringify(value));
+    window.location.href = "/taste?time=true&first_class=";
+  };
+  const ClickPm = (event) => {
+    event.preventDefault();
+    const value = [];
+    value.push(false);
+    sessionStorage.setItem("value", JSON.stringify(value));
+    window.location.href = "/taste?time=true&first_class=";
+  };
+  return (
+    <div className="container">
+      <div class="progress-bar-container">
+        <div class="progress-bar">
+          <div class="progress"></div>
+        </div>
+        <div class="dashed-line"></div>
+      </div>
+      <h1>
+        오전과 오후 수업 중,
+        <br />
+        어느 시간대를 더 선호하시나요?
+      </h1>
+      <div className="line"></div>
+      <div className="button-container">
+        <button onClick={clickAm} className="btn">
+          오전 수업이 좋아요
+        </button>
+        <button onClick={ClickPm} className="btn">
+          오후 수업이 좋아요
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Time;
