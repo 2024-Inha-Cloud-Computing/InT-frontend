2a38a06dc3c393e20142863f2373ba25e52a1bd5
kaswhy <sxshx818@naver.com>
2024-06-04 01:19:32 +0900

    design: 학교 인증 성공 여부 디자인 bug fix

--- src/components/join/SchoolCheck.js
+++ src/components/join/SchoolCheck.js
@@ -42,7 +42,7 @@ const SchoolCheck = () => {
 
   const checkScool = (event) => {
     event.preventDefault();
-    const span = document.querySelector("#message");
+    const span = document.querySelector("#schoolCheck_message");
     //확인하는 로직
     if (!school) {
       span.className = "identifyFail";
@@ -88,7 +88,7 @@ const SchoolCheck = () => {
             확인
           </button>
         </div>
-        <span id="message" className="hidden"></span>
+        <span id="schoolCheck_message" className="hidden"></span>
       </form>
       <button className="nextCheckButton" onClick={goNext}>
         다음으로 넘어가기
