ecfc32f3dc03cae6074e32cf41899ba6d53f1d85
wogus <ind07152@naver.com>
2024-06-09 13:29:46 +0900

    fix : timetable 다음 url 수정

--- src/pages/Timetable.js
+++ src/pages/Timetable.js
@@ -4,33 +4,35 @@ import "../css/Timetable.css";
 import back from "../assets/img/back.png";
 
 const Timetable = () => {
-    const goBack = () => {
-      window.location.href = "/homeAftLog";
-    };
-
-    const goManual = () => {
-      window.location.href = "/timetable/manual";
-    };
-
-    const goFiltering = () => {
-      window.location.href = "/timetable/subject";
-    }
-    return (
-      <div className="timetable_container">
-        <img className="goback" src={back} onClick={goBack}></img>
-        <div className="timetable_title1">InT는 시간표를 더욱 쉽게</div>
-        <div className="timetable_title2">짤 수 있는 기능을 제공해요!</div>
-        <div className="timetable_content">
-          <div className="timetable_line"></div>
-          <button className="timetable_filteringButton" onClick={goFiltering}>
-            네! InT가 제공하는 기능으로<br />맞춤형 시간표를 짜 볼래요.
-          </button>
-          <button className="timetable_manualButton" onClick={goManual}>
-            직접 짜는 게 더 편해요.
-          </button>
-        </div>
-      </div>
-    );
+  const goBack = () => {
+    window.location.href = "/homeAftLog";
   };
-  
-  export default Timetable;
+
+  const goManual = () => {
+    window.location.href = "/timetable/manual";
+  };
+
+  const goFiltering = () => {
+    window.location.href = "/firstFiltering";
+  };
+  return (
+    <div className="timetable_container">
+      <img className="goback" src={back} onClick={goBack}></img>
+      <div className="timetable_title1">InT는 시간표를 더욱 쉽게</div>
+      <div className="timetable_title2">짤 수 있는 기능을 제공해요!</div>
+      <div className="timetable_content">
+        <div className="timetable_line"></div>
+        <button className="timetable_filteringButton" onClick={goFiltering}>
+          네! InT가 제공하는 기능으로
+          <br />
+          맞춤형 시간표를 짜 볼래요.
+        </button>
+        <button className="timetable_manualButton" onClick={goManual}>
+          직접 짜는 게 더 편해요.
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Timetable;
